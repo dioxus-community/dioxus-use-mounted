2ef9d54ebfe6766b74ab69867e5ade5d5faa209a
Matt Hunzinger <matthunz2@gmail.com>
2023-10-17 22:32:05 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dioxus-use-mounted
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::rc::Rc;
+use dioxus::prelude::*;
+use dioxus_signals::{Signal, use_signal};
+
+pub fn use_mounted<T>(cx: Scope<T>) -> UseMounted {
+    let signal = use_signal(cx, || None);
+    UseMounted { signal }
+}
+
+#[derive(Clone, Copy)]
+pub struct UseMounted {
+    pub signal: Signal<Option<Rc<MountedData>>>
+}
+
+impl UseMounted {
+    pub fn onmounted(self, event: Event<MountedData>) {
+        self.mount(event.data)
+    }
+
+    pub fn mount(self, data: Rc<MountedData>) {
+        self.signal.set(Some(data));
+    }
+}
